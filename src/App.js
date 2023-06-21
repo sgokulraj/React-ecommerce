6ddd322098dd84f50f2090dc7b7498615fa1e7d4
sgokulraj <116162072+sgokulraj@users.noreply.github.com>
2023-06-21 12:29:26 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Cart from "./Components/Cart";
 import Error from "./Components/Error";
 import SignIn from "./Components/SignIn";
 import Register from "./Components/Register";
-import Profile from './Components/Profile';
+import Profile from "./Components/Profile";
+import Protected from "./Components/Protected";
 
 function App() {
-  return ( 
+  // const [isSignedIn, setIsSignedIn] = useState(null)
+  // const signin = () => {
+  //   setIsSignedIn(true)
+  // }
+  // const signout = () => {
+  //   setIsSignedIn(false)
+  // }
+  return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/cart"
+          element={
+            <Protected>
+              <Cart />
+            </Protected>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Protected>
+              <Profile />
+            </Protected>
+          }
+        />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
